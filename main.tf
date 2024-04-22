b38ade5d2537dabb37a36c11891a0d78786b2843
Shiva Kumar <shiva@Shivas-MacBook-Pro.local>
2024-04-22 09:27:03 -0700

    backing up the state in s3

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "shiva-terraform-bucket"
+    key    = "myapp/state.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
